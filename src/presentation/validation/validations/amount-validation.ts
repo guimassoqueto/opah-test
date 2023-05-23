7870d6f8f14e06079c9373759f0cf1468bc420da
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 15:41:46 -0300

    fix: corrige validação

--- src/presentation/validation/validations/amount-validation.ts
+++ src/presentation/validation/validations/amount-validation.ts
@@ -9,7 +9,10 @@ export class AmountValidation implements Validation {
   validate (input: any): Error | null {
     if (typeof input[this.fieldName] !== 'number') return new InvalidParamError(this.fieldName)
     if (input[this.fieldName] <= 0) return new InvalidParamError(this.fieldName)
-    if (input[this.fieldName].toString().split('.')[1].length > 2) return new InvalidParamError(this.fieldName)
+
+    const valueToString = input[this.fieldName].toString() as string
+    if (valueToString.includes('.') && valueToString.split('.')[1].length > 2) return new InvalidParamError(this.fieldName)
+
     return null
   }
 }
