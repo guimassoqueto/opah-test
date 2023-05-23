529dc21a9fb316133f738b466237e52fefc9ff8f
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 15:25:17 -0300

    refactor: renomeia nome da classe

--- src/presentation/controllers/credit-debit/credit-debit-controller.ts
+++ src/presentation/controllers/credit-debit/credit-debit-controller.ts
@@ -2,25 +2,27 @@ import { type Controller } from '../../interfaces/controller'
 import { type HttpRequest, type HttpResponse } from '../../types/http'
 import { badRequest, ok, serverError } from '../../helpers/common-responses'
 import { type Validation } from '../../interfaces/validation'
-import loggerConfig from '../../../logger-config'
 import { type AddTransaction } from '../../../domain/usecases/add-transaction'
+import { type TransactionType } from '../../../domain/models/transactions'
+import loggerConfig from '../../../logger-config'
 
-const logger = loggerConfig('DebitController')
+const logger = loggerConfig('CreditDebitController')
 
-export default class DebitController implements Controller {
+export default class CreditDebitController implements Controller {
   constructor (
     private readonly validation: Validation,
-    private readonly addTransaction: AddTransaction
+    private readonly addTransaction: AddTransaction,
+    private readonly transactionType: TransactionType
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    logger.info('New debit request...')
+    logger.info(`New ${this.transactionType === 'D' ? 'Debit' : 'Credit'} Request...`)
     try {
       const error = this.validation.validate(request.body)
       if (error) return badRequest(error)
 
       const { amount } = request.body
-      const transaction = await this.addTransaction.add(amount, 'D')
+      const transaction = await this.addTransaction.add(amount, this.transactionType)
 
       return ok(transaction)
     } catch (error) {
