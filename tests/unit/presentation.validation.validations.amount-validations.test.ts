94b0710897dedf34c19c64317c51ce93c15e50f5
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 15:30:39 -0300

    test: garante que retorna erro se o campo passado não for do tipo esperado

--- tests/unit/presentation.validation.validations.amount-validations.test.ts
+++ tests/unit/presentation.validation.validations.amount-validations.test.ts
@@ -11,4 +11,19 @@ describe('AmountValidation' , () => {
     expect(result).toBeInstanceOf(Error)
   })
 
+  test('Deve retornar erro quando a chave amount não for do tipo number', () => {
+    const amountValidation = new AmountValidation('amount')
+    const obj1 = { amount: '49.90' }
+    const result1 = amountValidation.validate(obj1)
+    expect(result1).toBeInstanceOf(Error)
+
+    const obj2 = { amount: ['49.90'] }
+    const result2 = amountValidation.validate(obj2)
+    expect(result2).toBeInstanceOf(Error)
+
+    const obj3 = { amount: true }
+    const result3 = amountValidation.validate(obj3)
+    expect(result3).toBeInstanceOf(Error)
+  })
+
 })
