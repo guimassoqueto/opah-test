2df08464c5da6702b2610ad0c8c9ca1a16b6e30b
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 13:17:48 -0300

    feat: adiciona classe de validação do amount da requisição

---
+++ src/presentation/validation/validations/amount-validation.ts
@@ -0,0 +1,14 @@
+import { InvalidParamError } from '../../../errors/invalid-param-error'
+import { type Validation } from '../../interfaces/validation'
+
+export class AmountValidation implements Validation {
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: any): Error | null {
+    if (input[this.fieldName] !== 'number') return new InvalidParamError(this.fieldName)
+    if (input[this.fieldName].toString().split('.')[1].length > 2) return new InvalidParamError(this.fieldName)
+    return null
+  }
+}
