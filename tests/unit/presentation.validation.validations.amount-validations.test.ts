057ccf6097c1a50ee03cbc7fc46b418c4b48e2b2
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 15:29:38 -0300

    test: garante que retorna erro se a chave nao estiver presente

--- tests/unit/presentation.validation.validations.amount-validations.test.ts
+++ tests/unit/presentation.validation.validations.amount-validations.test.ts
@@ -1,4 +1,14 @@
 import { AmountValidation } from "../../src/presentation/validation/validations/amount-validation"
 
 describe('AmountValidation' , () => {
+  test('Deve retornar erro quando a chave amount não estiver presente', () => {
+    const amountValidation = new AmountValidation('amount')
+    const obj = {
+      test: '123'
+    }
+    const result = amountValidation.validate(obj)
+
+    expect(result).toBeInstanceOf(Error)
+  })
+
 })
