84eaa2eba70c578525ddaf705b492cc9c58d31ed
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-23 15:27:39 -0300

    fix: corrige implementação

--- src/presentation/validation/validations/amount-validation.ts
+++ src/presentation/validation/validations/amount-validation.ts
@@ -7,7 +7,8 @@ export class AmountValidation implements Validation {
   ) {}
 
   validate (input: any): Error | null {
-    if (input[this.fieldName] !== 'number') return new InvalidParamError(this.fieldName)
+    if (typeof input[this.fieldName] !== 'number') return new InvalidParamError(this.fieldName)
+    if (input[this.fieldName] <= 0) return new InvalidParamError(this.fieldName)
     if (input[this.fieldName].toString().split('.')[1].length > 2) return new InvalidParamError(this.fieldName)
     return null
   }
