df2adbdeb729819fe04cba5e476e34cf8dbe1daf
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-24 16:23:53 -0300

    test: garante retorno 500 caso o controller lance erro durante o processamento

---
+++ tests/unit/presentation.controllers.get-balance-controller.test.ts
@@ -0,0 +1,39 @@
+import { GetCurrentBalance } from "../../src/domain/usecases/get-current-balance"
+import GetBalanceController from "../../src/presentation/controllers/transactions/get-balance-controller"
+import { HttpRequest } from "../../src/presentation/protocols"
+
+function makeGetCurrentBalance(): GetCurrentBalance {
+  class GetCurrentBalanceStub implements GetCurrentBalance {
+    async get(): Promise<number> {
+      return new Promise(resolve => resolve(0))
+    }
+  }
+  return new GetCurrentBalanceStub()
+}
+
+type SutType = {
+  sut: GetBalanceController,
+  getCurrentBalanceStub: GetCurrentBalance
+}
+
+function makeSut(): SutType {
+  const getCurrentBalanceStub = makeGetCurrentBalance()
+  const sut = new GetBalanceController(getCurrentBalanceStub)
+
+  return {
+    sut,
+    getCurrentBalanceStub
+  }
+}
+
+describe('GetBalanceController' , () => {
+  test('Deve retornar 500 caso o método getCurrentBalance lance erro', async () => {
+    const { sut, getCurrentBalanceStub } = makeSut()
+    const spyGet = jest.spyOn(getCurrentBalanceStub, "get").mockRejectedValueOnce(new Error("any-error"))
+    const request: HttpRequest = {}
+    const response = await sut.handle(request)
+    
+    expect(response.statusCode).toBe(500)
+  })
+
+})
