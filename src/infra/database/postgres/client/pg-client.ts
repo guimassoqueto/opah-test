56b07594044cd89b665a2df06b5e60ee8bab8abe
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-24 04:55:49 -0300

    feat: modifica cliente para classe singleton

--- src/infra/database/postgres/client/pg-client.ts
+++ src/infra/database/postgres/client/pg-client.ts
@@ -1,4 +1,4 @@
-import { Pool, type PoolConfig } from 'pg'
+import { Pool, type PoolConfig, type QueryResult } from 'pg'
 import { type TransactionModel } from '../../../../domain/models/transactions'
 import {
   POSTGRES_DB,
@@ -14,52 +14,38 @@ const poolConfig: PoolConfig = {
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
   database: POSTGRES_DB,
-  max: 20,
+  max: 1, // TODO: valor deve ser outro para prod
   idleTimeoutMillis: 3000,
   connectionTimeoutMillis: 5000
 }
 
-export const pgClient = new Pool(poolConfig)
-pgClient.connect()
-  .then(() => {})
-  .catch(error => { console.error(error) })
+export class PostgresClient {
+  public client: Pool = new Pool(poolConfig)
 
-export function transactionMapper (obj: any): TransactionModel {
-  if (!obj) throw new Error()
-  const { amount, ...rest } = obj
-  return Object.assign({}, rest, { amount: parseFloat(amount) }) as TransactionModel
+  private static instance: PostgresClient
+
+  private constructor () {}
+
+  public static getInstance (): PostgresClient {
+    if (!PostgresClient.instance) {
+      PostgresClient.instance = new PostgresClient()
+    }
+
+    return PostgresClient.instance
+  }
+
+  public async query (query: string, values?: any[]): Promise<QueryResult<any>> {
+    const data = await this.client.query(query, values)
+    return data
+  }
+
+  public async end (): Promise<void> {
+    await this.client.end()
+  }
+
+  public transactionMapper (obj: any): TransactionModel {
+    if (!obj) throw new Error()
+    const { amount, ...rest } = obj
+    return Object.assign({}, rest, { amount: parseFloat(amount) }) as TransactionModel
+  }
 }
-
-// Singleton
-// export class PostgresHelper {
-//   public client: PoolClient
-//   public uri: string
-
-//   private static instance: PostgresHelper
-
-//   private constructor () {}
-
-//   public static getInstance (): PostgresHelper {
-//     if (!PostgresHelper.instance) {
-//       PostgresHelper.instance = new PostgresHelper()
-//     }
-
-//     return PostgresHelper.instance
-//   }
-
-//   public async connect (): Promise<void> {
-//     this.client = await new Pool(poolConfig).connect()
-//   }
-
-//   public async query (query: string, values?: any[]): Promise<QueryResult<any>> {
-//     const data = await this.client.query(query, values)
-//     return data
-//   }
-
-//   // TODO: modificar
-//   public mapper<T>(obj: any): T {
-//     if (!obj) throw new Error()
-//     const { _id, ...rest } = obj
-//     return Object.assign({}, rest, { id: _id.toString() }) as T
-//   }
-// }
