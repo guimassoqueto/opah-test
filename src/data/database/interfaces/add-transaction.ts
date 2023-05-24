475ff705ab61f08c1d19bac7047cc1dd03d78ea9
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-24 08:29:11 -0300

    refactor: move o arquivo para outra pasta

--- src/data/database/interfaces/add-transaction.ts
+++
@@ -1,6 +0,0 @@
-import { type TransactionModel } from '../../../domain/models/transactions'
-import { type AddTransactionModel } from '../../../domain/usecases/add-transaction'
-
-export interface AddTransactionRepository {
-  add: (transaction: AddTransactionModel) => Promise<TransactionModel>
-}
