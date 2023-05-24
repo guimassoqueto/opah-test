a6b71d1f12df755a54927e395e4fb457d167607b
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-24 06:14:42 -0300

    feat: adiciona novas variáveis de ambiente

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,9 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const ENVIRONMENT = process.env.ENVIRONMENT ?? 'dev'
+
+export const APP_PORT = parseInt(process.env.APP_PORT ?? '8000')
+
 export const POSTGRES_HOST = process.env.POSTGRES_HOST ?? 'localhost'
 export const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT ?? '5432')
 export const POSTGRES_DB = process.env.POSTGRES_DB ?? 'postgres'
