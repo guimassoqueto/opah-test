eb562c3d1361734d8efc86ae330d6aee0c3fb984
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-25 14:38:35 -0300

    fix: corrige implementação da classe

--- src/presentation/validation/validations/date-validation.ts
+++ src/presentation/validation/validations/date-validation.ts
@@ -4,12 +4,13 @@ import { type IDateValidation } from '../interfaces/interface-date-validation'
 
 export class DateValidation implements Validation {
   constructor (
+    private readonly fieldName: string,
     private readonly validator: IDateValidation
   ) {}
 
   validate (input: any): Error | null {
-    const isValid = this.validator.isValid(input as string)
-    if (!isValid) return new InvalidParamError('date format')
+    const isValid = this.validator.isValid(input[this.fieldName])
+    if (!isValid) return new InvalidParamError(this.fieldName)
     return null
   }
 }
